de730c4acc5801ab4b4498ca49f56d1e289153e2
Orkhan Amirli <orkhanea@code.edu.az>
2021-11-30 14:16:32 +0400

    First

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -77,6 +77,26 @@ USE Task_29;
 -- INSERT INTO Nobel_Win (YEAR, [SUBJECT], WINNER, COUNTRY, CATEGORY ) VALUES(1987, 'Economics', 'Robert Solow', 'USA', 'Economist');
 -- INSERT INTO Nobel_Win (YEAR, [SUBJECT], WINNER, COUNTRY, CATEGORY ) VALUES(1994, 'Literature', 'Kenzaburo Oe', 'Japan', 'Linguist');
 
+-- CREATE TABLE Orders(
+--     ID INT PRIMARY KEY IDENTITY(1,1),
+--     Purch_amt DECIMAL(6,2) NOT NULL,
+--     Ord_date DATE NOT NULL,
+--     Customer_id INT FOREIGN KEY REFERENCES customer (customer_id),
+--     Salesman_id INT FOREIGN KEY REFERENCES salesman(salesman_id)
+-- )
+
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (150.5, '05/10/2012', 3, 5001)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (65.26, '05/10/2012', 1, 5000)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (110.5, '08/17/2012', 6, 5005)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (948.5, '09/10/2012', 3, 5001)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (2400.6, '07/27/2012', 2, 5000)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (5760, '09/10/2012', 1, 5000)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (1983.43, '10/10/2012', 5, 5003)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (2480.4, '10/10/2012', 6, 5005)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (250.45, '06/27/2012', 4, 5001)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (75.29, '08/17/2012', 7, 5004)
+-- INSERT INTO Orders (Purch_amt, Ord_date, Customer_id, Salesman_id) VALUES (3045.6, '04/12/2012', 1, 5000)
+
 
 
 
@@ -135,6 +155,82 @@ USE Task_29;
 -- CREATE TABLE text_table ([Text]ntext);
 -- INSERT INTO text_table ([Text]) VALUES ('This is SQL Exercise, Practice and Solutions');
 -- SELECT * FROM text_table;
+
+
+-- Tasks that I did on class.
+
+-- select YEAR,[SUBJECT] from Nobel_Win WHERE Winner = 'Dennis Gabor'
+
+-- select * from Nobel_Win
+
+-- CREATE TABLE MOVIE(
+--     ID INT PRIMARY KEY IDENTITY(1,1),
+--     [Name] NVARCHAR(20) not NULL,
+--     [Date] DATE NOT NULL
+-- )
+
+-- INSERT INTO MOVIE ([Name], [Date]) VALUES('Dag 1', '11/12/2015');
+-- INSERT INTO MOVIE ([Name], [Date]) VALUES('Avatar', '11/13/2015');
+-- INSERT INTO MOVIE ([Name], [Date]) VALUES('Tenet', '11/14/2015');
+
+-- SELECT * FROM MOVIE
+
+-- CREATE TABLE GENRE (
+--     ID INT PRIMARY KEY IDENTITY(1,1),
+--     [Name] NVARCHAR(20) not NULL
+-- )
+
+-- INSERT into GENRE ([Name]) VALUES('Criminal')
+-- INSERT into GENRE ([Name]) VALUES('Comedy')
+-- INSERT into GENRE ([Name]) VALUES('Action')
+
+-- CREATE TABLE GenreToMovie (
+--     ID INT PRIMARY KEY IDENTITY(1,1),
+--     MovieId INT FOREIGN KEY REFERENCES MOVIE(ID),
+--     GenreId INT FOREIGN KEY REFERENCES GENRE(ID)
+-- )
+
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(1, 1)
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(2, 1)
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(2, 2)
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(3, 2)
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(3, 3)
+-- INSERT into GenreToMovie (MovieId, GenreId) VALUES(3, 1)
+
+-- SELECT * FROM GenreToMovie
+
+-- SELECT  MOVIE.Name, MOVIE.Date, GENRE.Name  FROM GenreToMovie
+-- INNER JOIN MOVIE on GenreToMovie.MovieId = MOVIE.ID
+-- INNER JOIN GENRE on GenreToMovie.GenreId = GENRE.ID
+
+-- Tasks that i did on class end!
+
+
+
+
+
+-- Task 30.11.2021
+
+-- From the following tables write a SQL query to find the salesperson(s) and the customer(s) he handle. Return Customer Name, city, Salesman, commission.
+
+-- SELECT c.cust_name 'Customer Name', c.city 'Customer City', s.Name 'Salesman Name', s.Comission FROM customer c
+-- INNER JOIN salesman s on c.salesman_id = s.salesman_id
+
+-- From the following tables write a SQL query to find those salespersons who received a commission from the company more than 12%. Return Customer Name, Customer City, Salesman, Commission.
+-- SELECT c.cust_name 'Customer Name', c.city 'Customer City', s.Name 'Salesman Name', s.Comission FROM customer c
+-- INNER JOIN salesman s on c.salesman_id = s.salesman_id
+-- WHERE s.Comission > 0.12 
+
+--From the following tables write a SQL query to find those salespersons do not live in the same city where their customers live and received a commission from the company more than 12%. Return Customer Name, Customer city, Salesman, Salesman City, Commission.
+-- SELECT c.cust_name 'Customer Name', c.city 'Customer City', s.Name 'Salesman Name', s.City 'Salesman City', s.Comission FROM customer c
+-- INNER JOIN salesman s on c.salesman_id = s.salesman_id
+-- WHERE c.city != s.City and s.Comission > 0.12
+
+--Write a SQL statement to make a join on the tables salesman, customer and orders in such a form that the same column of each table will appear once and only the relational rows will come.
+-- SELECT Distinct o.customer_id, o.salesman_id FROM Orders o
+-- INNER JOIN  customer c on o.customer_id = c.customer_id
+-- INNER JOIN  salesman s on o.salesman_id = s.salesman_id
+
 
 
 
